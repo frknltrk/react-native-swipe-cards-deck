75b8c5278ba3b8276ff163c5beba1b1974a57564
Furkan Ünlütürk <unluturkfurkan@protonmail.com>
2022-05-30 02:46:19 +0300

    fix for the new package name

--- example/App.js
+++ example/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import SwipeCards from "react-native-swipe-cards-deck";
+import SwipeCards from "react-native-swipeable-cards";
 
 function Card({ data }) {
   return (
